1020f86f778183b725ed71cd49508f74f7eb53db
Rudolf Schmidt <me@rudionrails.com>
2013-02-04 23:49:19 +0100

    CHORE: Refactorings

--- jquery.taggable.js
+++ jquery.taggable.js
@@ -5,7 +5,7 @@
  *
  * @author Rudolf Schmidt <me@rudionrails.com>
  * @copyright Rudolf Schmidt 2011-2013
- * @license MIT <http://opensource.org/licenses/mit-license.php>
+ * @license MIT
  * @link http://www.github.com/rudionrails/jquery.taggable
  * @module jquery.taggable
  * @version 1.0.0
@@ -13,14 +13,39 @@
  */
 ;(function($) {
 
+  /*
+   * The default options for the taggable plugin
+   */
   $.taggableDefaultOptions = {
-    delimiter: ',',                   // delimiter for separating tags 
-    defaultText: 'Add a tag',         // default text on the input 
-    defaultRemoveText: 'Remove tag',  // title tag on tag remove link
-    minChars: 1,                      // at least one character must be typed to add a tag
-    autocomplete: false               // takes jquery-ui autocomplete options
+    // delimiter for separating tags
+    delimiter: ',',
+
+    // default text on the input
+    placeholder: 'Add a tag',
+
+    // title tag on tag remove link
+    defaultRemoveText: 'Remove tag',
+
+    // at least one character must be typed to add a tag
+    minChars: 1,
+
+    // takes jquery-ui autocomplete options
+    autocomplete: false,
+
+    // options for automatically growing the (replaced) input field
+    autoGrow: {
+      maxWidth: 1000,
+      minWidth: 0,
+      comfortZone: 10
+    }
   }
 
+  /*
+   * The jquery.taggable plugin applies to all matching elements.
+   *
+   * It takes an options hash which it merged with the default options (in case
+   * you want to override things).
+   */
   $.fn.taggable = function( options ) {
     var options = $.extend( $.taggableDefaultOptions, options );
 
@@ -29,30 +54,194 @@
     });
   };
 
-  // http://stackoverflow.com/questions/1288297/jquery-auto-size-text-input-not-textarea
-  $.fn.taggableAutoGrow = function(options) {
-    var options = $.extend({
-      maxWidth: 1000,
-      minWidth: 0,
-      comfortZone: 10
-    }, options );
 
-    $(this).each( function() {
-      var minWidth = options.minWidth || $(this).width(),
+  /*
+   * The taggable class is called for every selector separately.
+   */
+  var Taggable = function( original, options ) {
+    var _original   = $(original),
+        _options    = options,
+        _keys       = { ENTER: 13, SPACE: 32, TAB: 9, BACKSPACE: 8 };
+
+    /*
+     * Private placeholder text
+     * @type  {String}
+     */
+    _placeholder = $( _original ).attr('placeholder') || _options.placeholder;
+
+    /*
+     * Private replacement object
+     * @type  {Object}
+     */
+    _replacement = $('<div>', { 'class': 'taggable-wrapper', tabIndex: -1 }).data( 'taggable', this ).append(
+      $('<input>', { 'class': 'taggable-hidden', type: 'hidden', name: $(_original).attr('name') }),
+      $('<input>', { 'class': 'taggable-input', value: _placeholder })
+    );
+
+    /*
+     * Private replacement input
+     * @type  {Object}
+     */
+    _replacementInput = $( _replacement ).find( '.taggable-input' );
+
+    /*
+     * Private replacement hidden field
+     * @type  {Object}
+     */
+    _replacementHidden = $( _replacement ).find( '.taggable-hidden' );
+
+    /*
+     * Private function to fetch the tag delimiter
+     *
+     * @return {String} The delimiter value
+     */
+    _delimiter = function() {
+      return _options.delimiter.charCodeAt(0);
+    };
+
+    /*
+     * Private function to fetch the tags
+     *
+     * @return  {Array}   The list of tags
+     */
+    _tags = function() {
+      var tags = $( _replacement ).find( '.taggable-tag' ).map( function() {
+        return $.trim( $(this).text() );
+      }).get().join( _delimiter() );
+
+      return tags;
+    };
+
+    /*
+     * Private function to check if the passed element's value is blank or not
+     *
+     * @param   {Object}  element   The DOM element to check the value against
+     * @return  {Boolean} true or false
+     */
+    _isBlank = function( element ) {
+      return $.trim( $(element).val() ) == "" ? true : false;
+    };
+
+    /*
+     * Adds value as tag
+     *
+     * @param   {String}  val   The value to being added as tag
+     * @return  {undefined}
+     */
+    _add = function( val ) {
+      if( _options.minChars > $.trim(val).length ) return; // early exit
+
+      var tag = $( '<span>', { 'class': 'taggable-tag' } ).html( $.trim(val) ).
+        mouseover( function() {
+          $(this).addClass( 'hover' );
+        }).mouseout( function() {
+          $(this).removeClass( 'hover' );
+        });
+
+      var close = $( '<a>', { 'class': 'taggable-close', href: '#', title: _options.defaultRemoveText }).
+        html('&nbsp;').
+        click( function() {
+          _remove( tag );
+        }).appendTo( tag );
+
+      // add the tag
+      $( _replacementInput ).before( tag );
+
+      // add the value to the hidden field
+      _update();
+    };
+
+    /*
+     * Remove the given tag element or the last one (if none given)
+     *
+     * @params  {Object}  element   The element to remove
+     * @return  {undefined}
+     */
+    _remove = function( element ) {
+      var element = element || $( _replacement ).find( '.taggable-tag:last' );
+
+      $(element).remove();
+      _update();
+    };
+
+    /*
+     * Update the hidden field (for form submission)
+     *
+     * @return  {undefined}
+     */
+    _update = function() {
+      $( _replacementHidden ).val( _tags() );
+    };
+
+    /*
+     * Adds event listeners
+     *
+     * @return  {undefined}
+     */
+    _addEventListeners = function() {
+      // define some behaviour on replacement elements
+      $( _replacement ).on( 'focus', function(event) {
+        $( '.taggable-input', $(this) ).focus();
+      });
+
+      // remove default text on focus
+      $( _replacementInput ).on( 'focus', function(event) {
+        if( $.trim( $(this).val() ) == _placeholder ) {
+          $(this).val( '' );
+        }
+
+      // display default text on blue (if value is empty)
+      }).on( 'blur', function(event) {
+        if( _isBlank(this) ) {
+          $(this).val( _placeholder );
+        }
+
+      }).on( 'keydown keypress', function(event) {
+        console.log( event.which.toString() );
+
+        // do nothing if first key is space and element is empty
+        if( event.which == _keys.SPACE && _isBlank(this) ) {
+          return false;
+        };
+
+        // if delimiter (comma by default), tab or enter then add a tag
+        if( event.which == _delimiter() || event.which == _keys.TAB || event.which == _keys.ENTER ) {
+          // add and reset
+          _add( $(this).val() );
+          $(this).val( '' );
+
+          return false;
+
+        // if backspace, then remove last tag
+        } else if( event.which == _keys.BACKSPACE && _isBlank(this) ) {
+          _remove();
+        }
+
+      });
+    };
+
+    /*
+     * Applies the auto-grow feature to the input
+     *
+     * @see http://stackoverflow.com/questions/1288297/jquery-auto-size-text-input-not-textarea
+     * @return {undefined}
+     */
+    _autoGrow = function() {
+      var minWidth = options.minWidth || $( _replacementInput ).width(),
         val = '',
-        tester = $( '<tester />' ).css({
+        tester = $( '<tester>' ).css({
           position: 'absolute',
           top: -9999,
           left: -9999,
           width: 'auto',
-          fontSize: $(this).css('fontSize'),
-          fontFamily: $(this).css('fontFamily'),
-          fontWeight: $(this).css('fontWeight'),
-          letterSpacing: $(this).css('letterSpacing'),
+          fontSize: $( _replacementInput ).css('fontSize'),
+          fontFamily: $( _replacementInput ).css('fontFamily'),
+          fontWeight: $( _replacementInput ).css('fontWeight'),
+          letterSpacing: $( _replacementInput ).css('letterSpacing'),
           whiteSpace: 'nowrap'
         });
 
-      $(this).after( tester ).
+      $( _replacementInput ).after( tester ).
         on( 'keyup keydown blur update', function(event) {
           if( val === (val = $(this).val()) ) return; //exit if not changed
 
@@ -65,140 +254,43 @@
 
           // Calculate new width + whether to change
           var testerWidth = tester.width(),
-            newWidth = (testerWidth + options.comfortZone) >= minWidth ? testerWidth + options.comfortZone : minWidth,
+            newWidth = (testerWidth + _options.autoGrow.comfortZone) >= minWidth ? testerWidth + _options.autoGrow.comfortZone : minWidth,
             currentWidth = $(this).width(),
             isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth) || 
-              (newWidth > minWidth && newWidth < options.maxWidth);
+              (newWidth > minWidth && newWidth < _options.autoGrow.maxWidth);
 
           // Animate width
-          if (isValidWidthChange) $(this).width(newWidth); 
+          if (isValidWidthChange) $( _replacementInput ).width(newWidth); 
         }).
         focusout(); // trigger
-    });
-  };
+    };
 
-
-  // === Taggable class
-  var Taggable = function( original, options ) {
-    this.options = options;
-
-    this.replacement = $('<div>', { 'class': 'taggable-wrapper', tabIndex: -1 }).data( 'taggable', this ).append( 
-      $('<input>', { 'class': 'taggable-hidden', type: 'hidden', name: $(original).attr('name') }), 
-      $('<input>', { 'class': 'taggable-input', value: $(original).attr('placeholder') || options.defaultText })
-    );
-
-    // replace original with taggable elements
-    this._replace( original );
-
-    // autocomplete if applicable
-    if( options.autocomplete != false ) {
-      this._autocomplete();
-    }
-  };
-
-  // taggable instance methods
-  $.extend( Taggable.prototype, {
-    // add a new tag
-    add: function( val ) {
-      if( this.options.minChars > $.trim(val).length ) return; // early exit
-
-      // add the tag
-      $( '.taggable-input', this.replacement ).before( 
-        $( '<span>', { 'class': 'taggable-tag' } ).mouseover( function() {
-          $(this).addClass( 'hover' );
-        }).mouseout( function() {
-          $(this).removeClass( 'hover' );
-        }).append(
-          $.trim(val),
-          $( '<a>', { 'class': 'taggable-close', href: '#', title: this.options.defaultRemoveText }).
-            html('&nbsp;').
-            data( 'taggable', this ).
-            click( function() {
-              $(this).data( 'taggable' ).remove( $(this).parent() );
-            })
-        )
-      );
-
-      // add the value to the hidden field
-      this._update();
-    },
-
-    // removed the given tag element or the last one (if none given)
-    remove: function( element ) {
-      var element = element || $( '.taggable-tag:last', this.replacement );
-
-      $(element).remove();
-      this._update();
-    },
-
-
-    // replaces the original element with the taggable ones
-    _replace: function( original ) {
-      var _this   = this,
-          options = this.options;
-
+    /*
+     * Initialize jquery.taggable
+     *
+     * @return  {undefined}
+     */
+    _init = function() {
       // add default tags
-      $.each( $(original).val().split(options.delimiter), function(index, value) {
-        _this.add( value ); 
+      $.each( $(_original).val().split( _delimiter() ), function(index, value) {
+        _add( value );
       });
 
-      // define some behaviour on replacement elements
-      $( this.replacement ).on( 'focus', function(event) {
-        $( '.taggable-input', $(this) ).focus();
-      });
+      _addEventListeners();
+      _autoGrow();
 
-      // remove default text on focus
-      $( '.taggable-input', this.replacement ).on( 'focus', this, function(event) {
-        if( $.trim( $(this).val() ) == event.data.options.defaultText ) {
-          $(this).val( '' );
-        }
-
-      }).on( 'blur', this, function(event) {
-        // display default text on blue (if value is empty)
-        if( $.trim( $(this).val() ) == '' ) {
-          $(this).val( event.data.options.defaultText );
-        }
-      }).on( 'keypress', this, function(event) {
-        // do nothing if first key is space and element is empty
-        if( event.which == 32 && $.trim($(this).val()) == "" ) {
-          return false;
-        }
-
-        // if delimiter (comma by default), tab or return then add a tag
-        if( event.which == event.data.options.delimiter.charCodeAt(0) || event.which == 9 || event.which == 13 ) {
-          event.data.add( $(this).val() );
-          $(this).val( '' ); //reset value
-
-          return false;
-
-        // if backspace, then remove last tag
-        } else if( event.which == 8 && $.trim($(this).val()) == "" ) {
-          event.data.remove();
-        }
-      });
-
-      // let the input grow with the typed text
-      $( '.taggable-input', this.replacement ).taggableAutoGrow();
+      // autocomplete if applicable
+      if( _options.autocomplete != false && typeof $.fn.autocomplete === "function" ) {
+        $( _replacementInput ).autocomplete( _options.autocomplete );
+      };
 
       // add replacement and then remove the original
-      $(original).after(this.replacement).remove();
-    },
+      $(_original).after( _replacement ).remove();
+    };
 
-    _update: function() {
-      var values = $( '.taggable-tag', this.replacement ).map( function() {
-        return $.trim( $(this).text() );
-      }).get().join( this.options.delimiter );
-
-      $( '.taggable-hidden', this.replacement ).val( values );
-    },
-
-    _autocomplete: function() {
-      // jquery-ui autocomplete
-      if( typeof $.fn.autocomplete == "function" ) {
-        $( '.taggable-input', this.replacement ).autocomplete( this.options.autocomplete );
-      }
-    }
-  });
+    // replace original with taggable elements
+    _init();
+  };
 
 })(jQuery);
 
